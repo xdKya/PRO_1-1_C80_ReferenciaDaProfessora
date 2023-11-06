664e3f7ac95814fb5877a46be16992a282a6789e
xdKya <90745676+xdKya@users.noreply.github.com>
2023-11-06 17:19:00 -0300

    Update Meteors.js

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -37,34 +37,20 @@ export default class MeteorScreen extends Component {
     }
 
     renderItem = ({ item }) => {
-        let meteor = item
-        let bg_img, speed, size;
-        if (meteor.threat_score <= 30) {
-            bg_img = require("../assets/meteor_bg1.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 100
-        } else if (meteor.threat_score <= 75) {
-            bg_img = require("../assets/meteor_bg2.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 150
-        } else {
-            bg_img = require("../assets/meteor_bg3.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 200
-        }
+        
         return (
             <View>
                 <ImageBackground source={bg_img} style={styles.backgroundImage}>
                     <View styles={styles.gifContainer}>
-                        <Image source={speed} style={{ width: size, height: size, alignSelf: "center" }}></Image>
-                        <View>
-                            <Text style={[styles.cardTitle, { marginTop: 400, marginLeft: 50 }]}>{item.name}</Text>
-                            <Text style={[styles.cardText, { marginTop: 20, marginLeft: 50 }]}>Mais Próximo da Terra - {item.close_approach_data[0].close_approach_date_full}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diâmetro Mínimo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_min}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diâmetro Máximo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_max}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Velocidade (KM/H) - {item.close_approach_data[0].relative_velocity.kilometers_per_hour}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Distância da Terra (KM) - {item.close_approach_data[0].miss_distance.kilometers}</Text>
-                        </View>
+                        
+                        // <View>
+                        //     <Text style={[styles.cardTitle, { marginTop: 400, marginLeft: 50 }]}>{item.name}</Text>
+                        //     <Text style={[styles.cardText, { marginTop: 20, marginLeft: 50 }]}>Mais Próximo da Terra - {item.close_approach_data[0].close_approach_date_full}</Text>
+                        //     <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diâmetro Mínimo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_min}</Text>
+                        //     <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diâmetro Máximo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_max}</Text>
+                        //     <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Velocidade (KM/H) - {item.close_approach_data[0].relative_velocity.kilometers_per_hour}</Text>
+                        //     <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Distância da Terra (KM) - {item.close_approach_data[0].miss_distance.kilometers}</Text>
+                        // </View>
                     </View>
                 </ImageBackground>
             </View>
